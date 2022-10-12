057ecb8a076878ef5555e85adf619e1a085ee80f
bl4zeee <115346818+bl4zeee@users.noreply.github.com>
2022-10-12 17:50:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-a=input()
-if len(s)!=16
-  print('ошибка')
-else: 
-  sum1=0
-  for v in range(0, len(a),2):
-      n=int(a[v])
-      if n*2>9
-          sum1=sum1(n*2-9)
-      else:
-          sum1=sum1+n*2
-  sum2=0
-  for z in range(1, len(s),2):
-      m=int(s[z])
-      sum2=sum2+m
-  n=sum1+sum2
-  if n%10==0:
-      print('карта корректна')
-  else:
-    print('ошибка')
+s=input()
+s.isdigit()
+sum2=0
+sum1=0
+if s.isdigit():
+    if len(s) == 16:
+        for i in s[0:len(s):2]:
+            if int(i) * 2 >=10 :
+                sum2+= 2 * int(i)%10 + 2 * int(i)//10
+            else:
+                sum2+=int(i)*2
+        for i in s[1:len(s):2]:
+            sum1+=int(i)
+        if (sum1+sum2)%10 == 0 and int(s)!=0:
+            print("карта корректнм")
+        else:
+            print("карта некорректна")
+            print(sum2)
+    else:
+        print("Карта неикорреткна")
+else:
+    print("ошибка")
